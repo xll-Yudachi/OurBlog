11c46c330dfe8827c35c35d921627d3f5ddb8696
yudachi <452419829@qq.com>
2021-01-21 11:50:19 +0800

    项目初始化

---
+++ ourblog-parent/ourblog-user/src/main/java/com/ourblog/user/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.ourblog.user.repository;
+
+import com.ourblog.common.bean.user.User;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @ClassName UserRepository
+ * @Description 用户持久层
+ * @Author Yudachi
+ * @Date 2021/1/21 10:37
+ * @Version 1.0
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
---
+++ ourblog-parent/ourblog-user/src/main/java/com/ourblog/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ourblog.user.service.impl;
+
+import com.ourblog.common.bean.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.ourblog.user.repository.UserRepository;
+import com.ourblog.user.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @ClassName UserServiceImpl
+ * @Description 用户接口实现类
+ * @Author Yudachi
+ * @Date 2021/1/21 10:39
+ * @Version 1.0
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User saveUser(User user){
+        User save = userRepository.save(user);
+        return save;
+    }
+}
